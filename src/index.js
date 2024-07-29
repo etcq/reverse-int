16da4915f4744a7276239218564c7832586e793e
Anton Sushnikov <resonans93@gmail.com>
2024-07-30 04:08:44 +0500

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let nString = n.toString();
+    if (nString[0] === '-') {
+      nString = nString.slice(1);
+    }
+    
+    return Number(nString.split('').reverse().join(''))
 }
